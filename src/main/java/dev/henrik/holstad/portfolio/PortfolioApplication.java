e0d54d01e89a0f3893e9bdbea28d2e6ca675e4f7
Henrik Holstad <1565871@gmail.com>
2024-08-30 21:32:07 +0200

    feat(setup): initial spring boot setup

---
+++ src/main/java/dev/henrik/holstad/portfolio/PortfolioApplication.java
@@ -0,0 +1,13 @@
+package dev.henrik.holstad.portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortfolioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/henrik/holstad/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.henrik.holstad.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
