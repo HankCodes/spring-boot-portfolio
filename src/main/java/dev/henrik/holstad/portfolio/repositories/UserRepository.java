1da2a6977e286d4e28e9099f809b8f7994c68e52
Henrik Holstad <1565871@gmail.com>
2024-09-07 22:27:43 +0200

    Updates API from User to Ticket

--- src/main/java/dev/henrik/holstad/portfolio/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.henrik.holstad.portfolio.repositories;
-
-
-import dev.henrik.holstad.portfolio.dao.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Integer> {
-
-}
